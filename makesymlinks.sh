5822c0215deaabf029b578065bac3d32a9052bb7
Cao Quang Binh <binhcq@asiantech.vn>
2016-07-07 11:40:47 +0700

    [Update] script
    - Add zsh plugins

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -23,10 +23,11 @@ cd $dir
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
+    echo "Moving $file from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
+    echo "Creating symlink for $file"
     ln -s $dir/$file ~/.$file
 done
 
@@ -36,6 +37,7 @@ if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     # Clone my oh-my-zsh repository from GitHub only if it isn't already present
     if [[ ! -d $dir/oh-my-zsh/ ]]; then
         git clone http://github.com/robbyrussell/oh-my-zsh.git
+        clone_oh_my_zsh_plugins
     fi
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
@@ -62,4 +64,19 @@ else
 fi
 }
 
+clone_oh_my_zsh_plugins() {
+  git clone git://github.com/zsh-users/zsh-autosuggestions.git oh-my-zsh/custom/plugins/zsh-autosuggestions
+  git clone git://github.com/zsh-users/zsh-syntax-highlighting.git oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+}
+
+make_git_alias() {
+  echo -n "Making git alias ~ ..."
+  git config --global alias.co checkout
+  git config --global alias.br branch
+  git config --global alias.ci commit
+  git config --global alias.st status
+  echo "done!"
+}
+
+make_git_alias
 install_zsh
--- vimrc
+++ vimrc
@@ -110,5 +110,3 @@ set hlsearch        " Highlight searches by default
 set ignorecase      " Ignore case when searching...
 set smartcase       " ...unless we type a capital
 
-" ================ Custom Settings ========================
-so ~/.yadr/vim/settings.vim
